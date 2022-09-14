e672b01b4d9688190e7d2740eaefc8181563d1ee
Maicon Lino <maicon@lifecti.com.br>
2022-09-14 10:00:20 -0300

    Teste dos atributos

--- streamlitapp.py
+++ streamlitapp.py
@@ -222,6 +222,9 @@ def main():
             st.header('Cura')
             st.metric(label='Probabilidade',value=str(prognosis[1])+'%')
             st.text("Atributos que influenciaram para este resultado por ordem de importância")
+
+            st.text(prognosis[2])
+            
             st.table(prognosis[2])
         else:
             st.header('Óbito')
